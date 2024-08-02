e356136fd1ed5a3c901d1d9e74c21883f6d47d87
Calvin Wong <cw161@rice.edu>
2024-08-02 01:23:47 -0500

    tsdx

--- cameraexample/cameraexample.tsx
+++ cameraexample/cameraexample.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import { Camera, CameraType } from 'react-camera-pro';
 
